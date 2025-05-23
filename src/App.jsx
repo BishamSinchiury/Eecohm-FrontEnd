7dfd6af0aecd87264b04d5e1e535db5a92581180
BishamSinchiury <bishamsinchiury1116@gmail.com>
2025-05-23 13:35:12 +0545

    intial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Navbar from './Component/NavBar/NavBar'
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './NavBar.css'; // Link to the CSS file
+
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <>
+    <nav className='navbar'>
+
+    </nav>
+    </>
+  );
+};
+
+export default Navbar;
